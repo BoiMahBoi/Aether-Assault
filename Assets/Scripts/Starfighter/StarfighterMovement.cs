df5235448632d7b52c36f9d2ce0ac7ed9507719d
ChE5HiR3 <sasjastedalbuch@gmail.com>
2022-10-12 20:44:39 +0200

    movement starfighter

--- Assets/Scripts/Starfighter/StarfighterMovement.cs
+++ Assets/Scripts/Starfighter/StarfighterMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class StarfighterMovement : MonoBehaviour
 {
+    public float Speed;
+    public float rotateSpeed;
+    private Rigidbody2D Rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody = GetComponent<Rigidbody2D>();
+        Speed = 200f;
+        rotateSpeed = 2f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow)) 
+        {
+            Rigidbody.AddForce(transform.up * Time.deltaTime * Speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            transform.Rotate(new Vector3(0, 0, rotateSpeed) * Time.deltaTime * Speed, Space.World);   
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, -rotateSpeed) * Time.deltaTime * Speed, Space.World);
+        }
     }
 }
