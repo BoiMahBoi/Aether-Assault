eda02504bd46505c5f30f9b3b1597a83767c74a7
ChE5HiR3 <sasjastedalbuch@gmail.com>
2022-11-03 10:47:34 +0100

    guideliner

---
+++ Assets/Scripts/WallSystem (Sasja/LineGuider.cs
@@ -0,0 +1,67 @@
+using JetBrains.Annotations;
+using Mono.Cecil.Cil;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineGuider : MonoBehaviour
+{
+    //we make a linerenderer
+    private LineRenderer lineRenderer;
+    private bool outOfGame;
+    public GameObject player;
+    public GameObject planet;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outOfGame = false; 
+        //we make a reference to our linerenderer and we get the component, so we can use it
+        lineRenderer = GetComponent<LineRenderer>();
+        //we set it to two points
+        lineRenderer.positionCount = 2;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if we are not in game
+        if (outOfGame) 
+        {
+            //we draw a line from position 1, which is our planet 
+            lineRenderer.SetPosition(0,planet.transform.position);
+            //to our player
+            lineRenderer.SetPosition(1, player.transform.position); 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if the game object we collided with has the tag levelboarders, then we
+        if(collision.gameObject.CompareTag("levelboarders")) 
+        {
+            //are out of the game and the bool = false, means it draw a line to guide us back.
+            outOfGame = false;
+
+            lineRenderer.enabled = false;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("levelboarders"))
+        {
+            outOfGame = true;
+
+            lineRenderer.enabled = true;
+        }
+    }
+
+
+}
