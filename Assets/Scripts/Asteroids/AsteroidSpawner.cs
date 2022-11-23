3cb32fed7cd6bf0f89bca3a70a4ab733402385d3
BoiMahBoi <mtr0607@hotmail.dk>
2022-11-23 20:16:13 +0100

    AsteroidSpawner

--- Assets/Scripts/Asteroids/AsteroidSpawner.cs
+++ Assets/Scripts/Asteroids/AsteroidSpawner.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-
     public GameObject[] asteroidPrefabs;
 
     [Header("Coordinates")]
@@ -20,14 +19,37 @@ public class AsteroidSpawner : MonoBehaviour
 
     public IEnumerator spawnAsteroid()
     {
+        int spawnPicker = Random.Range(1, 4);
+
         int randomPrefab = Random.Range(0, asteroidPrefabs.Length);
-        float randX = Random.Range(-mapX, mapX);
-        float randY = Random.Range(-mapY, mapY);
-        Vector2 randLoc = new Vector2(randX, randY);
+
+        float randXAbove = Random.Range(50, mapX);
+        float randXBelow = Random.Range(-50, -mapX);
+        float randYAbove = Random.Range(30, mapY);
+        float randYBelow = Random.Range(-30, -mapY);
+
         float randRotation = Random.Range(0, 359);
         Quaternion randRot = new Quaternion(0, 0, randRotation, 0);
-        Instantiate(asteroidPrefabs[randomPrefab], randLoc, randRot, transform);
-        int randomTime = Random.Range(1, 5);
+
+        if(spawnPicker == 1)
+        {
+            Vector2 randLoc = new Vector2(randXAbove, randYAbove);
+            Instantiate(asteroidPrefabs[randomPrefab], randLoc, randRot, transform);
+        } else if (spawnPicker == 2)
+        {
+            Vector2 randLoc = new Vector2(randXAbove, randYBelow);
+            Instantiate(asteroidPrefabs[randomPrefab], randLoc, randRot, transform);
+        } else if (spawnPicker == 3)
+        {
+            Vector2 randLoc = new Vector2(randXBelow, randYAbove);
+            Instantiate(asteroidPrefabs[randomPrefab], randLoc, randRot, transform);
+        } else
+        {
+            Vector2 randLoc = new Vector2(randXBelow, randYBelow);
+            Instantiate(asteroidPrefabs[randomPrefab], randLoc, randRot, transform);
+        }
+
+        int randomTime = Random.Range(3, 10);
         yield return new WaitForSeconds(randomTime);
         StartCoroutine(spawnAsteroid());
     }
